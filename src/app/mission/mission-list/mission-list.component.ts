53e9f6163ed0e2642caee2d74c05089f734e1f9f
Andrew Connell <me@andrewconnell.com>
2017-05-15 12:32:10 -0400

    checkpoint 02

---
+++ src/app/mission/mission-list/mission-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+
+import { MissionService } from '../mission.service';
+import { IMission } from '../../models';
+
+@Component({
+  selector: 'app-mission-list',
+  templateUrl: './mission-list.component.html',
+  styleUrls: ['./mission-list.component.scss']
+})
+export class MissionListComponent implements OnInit {
+
+  missions: IMission[];
+
+  constructor(private missionService: MissionService) { }
+
+  ngOnInit() {
+    this._getMissions();
+  }
+
+  private _getMissions(): void {
+    this.missions = this.missionService.getMissions();
+  }
+
+}
---
+++ src/app/mission/mission.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MissionListComponent } from './mission-list/mission-list.component';
+import { MissionService } from './mission.service';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    MissionListComponent
+  ],
+  exports: [
+    MissionListComponent
+  ],
+  providers: [MissionService]
+})
+export class MissionModule { }
